4f2208b72b5eaa15fe947ef65748dcb5b67c2de2
daCruzZz_Leticia <dacruzzzleticia@gmail.com>
2025-04-28 09:19:49 -0300

    feat: Adiciona submissão 1001

---
+++ iniciante-beginner/1001/Main.java
@@ -0,0 +1,30 @@
+
+/**
+ * Source: Beecrowd
+ * @problem: 1001 - Extremely Basic
+ * @subject: Sequential
+ * 
+ * @author: Letícia Cruz
+ * @date: 2025-02-24
+ */
+
+import java.io.IOException; // uso padrão no beecrowd para java
+import java.util.Scanner; // importa o scanner, que é usado para ler entrada
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        // Lê entrada, declara váriáveis e atribui valores
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        int X = A + B; // Calcula soma
+
+        System.out.println("X = " + X); // Imprime soma
+
+        sc.close();
+    }
+
+}
