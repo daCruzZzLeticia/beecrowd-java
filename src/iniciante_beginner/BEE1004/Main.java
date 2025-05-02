a49c0a73426dd0c7d8cf549b3c7105a9971ea716
daCruzZz_Leticia <dacruzzzleticia@gmail.com>
2025-05-02 17:55:55 -0300

    feat: Adiciona submissão 1004

---
+++ src/iniciante_beginner/BEE1004/Main.java
@@ -0,0 +1,30 @@
+package iniciante_beginner.BEE1004;
+
+/**
+ * Source: Beecrowd
+ * @problem: 1004 - Simple Product
+ * @subject: Sequential
+ * 
+ * @author: Letícia Cruz
+ * @date: 2025-04-18
+ */
+
+import java.io.IOException; // uso padrão do beecrowd para java
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        // Lê entrada, declara váriáveis e atribui valores
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        int PROD = A * B; // calcula produto
+
+        System.out.println("PROD = " + PROD); // imprime produto
+
+        sc.close();
+
+    }
+}
