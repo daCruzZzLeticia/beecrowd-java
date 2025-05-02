3e31a527821425fec783a79351e8e0d3646edae4
daCruzZz_Leticia <dacruzzzleticia@gmail.com>
2025-05-02 17:56:48 -0300

    feat: Adiciona submissão 1005

---
+++ src/iniciante_beginner/BEE1005/Main.java
@@ -0,0 +1,31 @@
+package iniciante_beginner.BEE1005;
+
+/**
+ * Source: Beecrowd
+ * @problem: 1005 - Average 1
+ * @subject: Sequential
+ * 
+ * @author: Letícia Cruz
+ * @date: 2025-04-18
+ */
+
+import java.io.IOException; // uso padrão do beecrowd para java
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        // Lê entrada, declara váriáveis e atribui valores
+        Scanner sc = new Scanner(System.in);
+        double A = sc.nextDouble();
+        double B = sc.nextDouble();
+
+        // Cálculo da media: multiplica peso com nota correpondente e divide tudo pela
+        // soma dos pesos
+        double MEDIA = (A * 3.5 + B * 7.5) / 11;
+
+        System.out.printf("MEDIA = %.5f\n", MEDIA); // imprime média com 5 casas decimais
+
+        sc.close();
+    }
+}
