dc33d7d9b17532430fa6c08f2b4a96e2e347c877
daCruzZz_Leticia <dacruzzzleticia@gmail.com>
2025-05-03 13:56:33 -0300

    feat: adiciona submissão 1848

--- README.md
+++ README.md
@@ -13,6 +13,8 @@ Este repositório contém minhas soluções para os problemas do Beecrowd (antig
 -   [1004 - Simple Product](src/iniciante_beginner/BEE1004/Main.java)
 -   [1005 - Average 1](src/iniciante_beginner/BEE1005/Main.java)
 -   [1010 - Simple Calculate](src/iniciante_beginner/BEE1010/Main.java)
+-   [1013 - The Greatest](src/iniciante_beginner/BEE1013/Main.java)
+-   [1048 - Counting Crow](src/iniciante_beginner/BEE1848/Main.java)
 
 ## Estrutura/Structure
 
---
+++ src/iniciante_beginner/BEE1848/Main.java
@@ -0,0 +1,58 @@
+package iniciante_beginner.BEE1848;
+
+/**
+ * Source: Beecrowd
+ * @problem: 1048 - Counting Crow
+ * @subject: Sequential
+ * 
+ * @author: Letícia Cruz
+ * @date: 2025-04-21
+ */
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        int contadorGritos = 0;
+        String binarioStr = "";
+
+        int soma = 0;
+        while (contadorGritos < 3) {
+
+            String linha = sc.nextLine();
+            String primeiroChar = String.valueOf(linha.charAt(0));
+
+            // verifica se o primeiro char da linha é c (de: cow cow)
+            if (primeiroChar.equals("c")) {
+                contadorGritos++;
+                System.out.println(soma);
+
+                // reseta valores:
+                binarioStr = "";
+                soma = 0;
+            } else {
+                for (int i = 0; i < 3; i++) {
+                    String c = String.valueOf(linha.charAt(i));
+                    if (c.equals("*")) {
+                        binarioStr += "1";
+                    } else {
+                        binarioStr += "0";
+                    }
+                }
+                // passa a string de 0 e 1 para decimal na base numércia 2 (binária):
+                int decimal = Integer.parseInt(binarioStr, 2);
+
+                soma += decimal;
+                binarioStr = ""; // reseta para próxima string
+            }
+
+        }
+
+        sc.close();
+    }
+
+}
