863278bf52b4b65f0cc6d8335d40a89b40e4be1f
daCruzZz_Leticia <dacruzzzleticia@gmail.com>
2025-05-03 13:54:16 -0300

    feat: adiciona submissão 1013

---
+++ src/iniciante_beginner/BEE1013/Main.java
@@ -0,0 +1,39 @@
+package iniciante_beginner.BEE1013;
+
+/**
+ * Source: Beecrowd
+ * @problem: 1013 - The Greatest
+ * @subject: Sequential
+ * 
+ * @author: Letícia Cruz
+ * @date: 2025-04-18
+ */
+
+import java.io.IOException;
+import java.util.Scanner;
+import static java.lang.Math.abs;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        // Lê entrada, declara váriáveis e atribui valores
+        Scanner sc = new Scanner(System.in);
+        String[] linha1 = (sc.nextLine()).split(" ");
+        int a = Integer.parseInt(linha1[0]);
+        int b = Integer.parseInt(linha1[1]);
+        int c = Integer.parseInt(linha1[2]);
+
+        // calcula maior entre A e B
+        int maiorAB = (a + b + abs(a - b)) / 2;
+
+        // usa o maior entre A e B da linha anterior e calcula com C:
+        int maiorABC = (maiorAB + c + abs(maiorAB - c)) / 2;
+
+        System.out.printf("%d eh o maior\n", maiorABC); // imprime o maior de A, B e C
+
+        sc.close();
+
+    }
+
+}
