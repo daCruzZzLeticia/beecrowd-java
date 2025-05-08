0c201f6c3eb99afab3d618268a6b6a28c2d2a1fb
daCruzZz_Leticia <dacruzzzleticia@gmail.com>
2025-05-08 07:21:06 -0300

    feat: adiciona submissão 1009

---
+++ src/iniciante_beginner/BEE1009/Main.java
@@ -0,0 +1,32 @@
+package iniciante_beginner.BEE1009;
+
+/**
+ * Source: Beecrowd
+ * @problem: 1009 - Salary with Bonus
+ * @subject: Sequential
+ * 
+ * @author: Letícia Cruz
+ * @date: 2025-04-18
+ */
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+
+        String nome = sc.nextLine();
+        double salarioFixo = sc.nextDouble();
+        double vendasEfetuadas = sc.nextDouble();
+
+        double totalReceber = salarioFixo + vendasEfetuadas * 0.15;
+        System.out.printf("TOTAL = R$ %.2f\n", totalReceber);
+
+        sc.close();
+
+    }
+
+}
