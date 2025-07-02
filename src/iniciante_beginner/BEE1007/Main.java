819a314c4023c95dd5ed43bc4414430fa72293e8
daCruzZz_Leticia <dacruzzzleticia@gmail.com>
2025-07-02 10:02:43 -0300

    feat: adiciona submissão 1007

---
+++ src/iniciante_beginner/BEE1007/Main.java
@@ -0,0 +1,22 @@
+package iniciante_beginner.BEE1007;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int d = scanner.nextInt();
+
+        int diferenca = (a * b - c * d);
+
+        System.out.println("DIFERENCA = " + diferenca);
+
+        scanner.close();
+
+    }
+}
