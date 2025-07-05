2ffedfffaba27507273ca17950ebb9047bf2516e
daCruzZz_Leticia <dacruzzzleticia@gmail.com>
2025-07-05 12:55:56 -0300

    feat: adiciona submissão 1006

---
+++ src/iniciante_beginner/BEE1006/Main.java
@@ -0,0 +1,29 @@
+package iniciante_beginner.BEE1006;
+
+/**
+ * Source: Beecrowd
+ * @problem: 1006 - Average 2
+ * @subject: Sequential
+ * 
+ * @author: Letícia Cruz
+ * @date: 2025-07-05
+ */
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+        double c = scanner.nextDouble();
+
+        double media = (a * 2 + b * 3 + c * 5) / 10;
+
+        System.out.printf("MEDIA = %.1f\n", media);
+
+        scanner.close();
+    }
+}
